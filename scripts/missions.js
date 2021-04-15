5f9cfff24ffde63b4e7231e97ac2c3aced05c945
swavyfeel <lucasolivaresperez@gmail.com>
2021-04-15 22:23:50 +0100

    Added play button

--- scripts/missions.js
+++ scripts/missions.js
@@ -30,16 +30,19 @@ var missionPanel = document.getElementById('missionPanel');
 
 for (mission of missions) {
     var div = document.createElement('div');
+    var divText = document.createElement('div');
     var pName = document.createElement('p');
     var pDescription = document.createElement('p');
     var tName = document.createTextNode(mission['name']);
     var tDescription = document.createTextNode(mission['description']);
     div.className = 'mission';
+    divText.className = 'missionText';
     pName.className = 'missionName';
     pDescription.className = 'missionDescription';
     pName.appendChild(tName);
     pDescription.appendChild(tDescription);
-    div.appendChild(pName);
-    div.appendChild(pDescription);
+    divText.appendChild(pName);
+    divText.appendChild(pDescription);
+    div.appendChild(divText);
     missionPanel.appendChild(div);
 }
--- scripts/playButton.js
+++ scripts/playButton.js
@@ -3,13 +3,20 @@ playButton.className = "playButton";
 playButton.appendChild(document.createTextNode("JUGAR"));
 
 var setPlayButton = function(event) {
+    if (event.target === playButton) {
+        return;
+    }
     missions = document.getElementsByClassName("mission");
     for (mission of missions) {
         if (mission.contains(playButton)) {
             mission.removeChild(playButton);
         }
     }
-    event.target.appendChild(playButton);
+    target = event.target;
+    while (target.className !== 'mission') {
+        target = target.parentElement;
+    }
+    target.appendChild(playButton);
 }
 
 missions = document.getElementsByClassName("mission");
