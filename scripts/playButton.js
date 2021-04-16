a909d4cb7ac4f36305e883ed508ec0baa5046f3f
lucasolip <47455265+swavyfeel@users.noreply.github.com>
2021-04-16 10:31:01 +0100

    Improved dynamic styling of missions

--- scripts/playButton.js
+++ scripts/playButton.js
@@ -2,21 +2,29 @@ playButton = document.createElement('button');
 playButton.className = "playButton";
 playButton.appendChild(document.createTextNode("JUGAR"));
 
+playButtonHolder = null;
+
 var setPlayButton = function(event) {
     if (event.target === playButton) {
         return;
     }
     missions = document.getElementsByClassName("mission");
-    for (mission of missions) {
+    if (playButtonHolder) {
+        playButtonHolder.removeChild(playButton);
+        playButtonHolder.className = 'mission';
+    }
+    /*for (mission of missions) {
         if (mission.contains(playButton)) {
             mission.removeChild(playButton);
         }
-    }
+    }*/
     target = event.target;
     while (target.className !== 'mission') {
         target = target.parentElement;
     }
     target.appendChild(playButton);
+    target.className = 'missionActive';
+    playButtonHolder = target;
 }
 
 missions = document.getElementsByClassName("mission");
